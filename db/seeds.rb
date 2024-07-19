d841470d876829ab06a07bd09d1f678bb5cc694c
Daniel Alves <dalima.dev@gmail.com>
2024-07-19 10:03:05 -0300

    implements users creation on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'started seed process'
+
+User.roles.keys.each { |role|
+ User.create first_name: role, email: "#{role}@test.com", password: '123456', role:, confirmed_at: Time.current }
+
+puts 'finished seed process'
---
+++ test/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { '123456' }
+    confirmed_at { Time.current }
+  end
+end
