6dae7726916b73b3ab5821fa443c70fbf90c3eaf
Daniel Alves <dalima.dev@gmail.com>
2024-07-19 09:39:48 -0300

    add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :trackable, :confirmable
 
+  enum :role, admin: 0
+
   def name
     "#{first_name} #{last_name}".strip
   end
